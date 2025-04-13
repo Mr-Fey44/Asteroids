9a5dba6799c73593d3b435dc4eda5231910e79c1
Mr_Fey44 <lucasmain44@tutamail.com>
2025-04-12 19:28:35 -0600

    main now displays a black screen

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame as pg
+import constants as con
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((con.SCREEN_WIDTH, con.SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f'Screen width: {con.SCREEN_WIDTH}')
+    print(f'Screen height: {con.SCREEN_HEIGHT}')
+    H = 0
+    while H == 0:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.Surface.fill(screen, color = "black", rect = None)
+        pg.display.flip()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
