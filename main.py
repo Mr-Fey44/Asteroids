04beea94e4a49a5bbef02b21bf597a04ec93c58f
Mr_Fey44 <lucasmain44@tutamail.com>
2025-04-12 19:38:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import constants as con
 def main():
     pg.init()
     screen = pg.display.set_mode((con.SCREEN_WIDTH, con.SCREEN_HEIGHT))
-
+    clock = pg.time.Clock()
+    dt = 0
     H = 0
     while H == 0:
         for event in pg.event.get():
@@ -15,6 +16,8 @@ def main():
                 return
         pg.Surface.fill(screen, color = "black", rect = None)
         pg.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
 
 
 
